5edfb6a685370fc22e7e3e469c364d7b85d169bf
smallmuou <lvyexuwenfa100@126.com>
2013-10-15 10:29:08 +0800

    修改为MIT

--- RTSPPlayer/RTSPPlayer/RTSPPlayer.h
+++ RTSPPlayer/RTSPPlayer/RTSPPlayer.h
@@ -1,7 +1,21 @@
-
 /*!
- * Copyright (c) 2013,福建星网视易信息系统有限公司
- * All rights reserved.
+   Permission is hereby granted, free of charge, to any person obtaining a copy
+   of this software and associated documentation files (the "Software"), to deal
+   in the Software without restriction, including without limitation the rights
+   to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+   copies of the Software, and to permit persons to whom the Software is
+   furnished to do so, subject to the following conditions:
+   
+   The above copyright notice and this permission notice shall be included
+   in all copies or substantial portions of the Software.
+   
+   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+   AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+   OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+   THE SOFTWARE.
  
  * @File:       RTSPPlayer.h
  * @Abstract:   RTSP 流播放（整理https://github.com/hackacam/ios_rtsp_player,版权由原作者保留）
